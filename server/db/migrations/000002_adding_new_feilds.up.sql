54ab705f188ac521e89b2c9ce9a88e2160a77399
zuhaibdev <msuhaib637@gmail.com>
2026-04-13 23:18:27 +0300

    Update 000002_adding_new_feilds.up.sql

--- server/db/migrations/000002_adding_new_feilds.up.sql
+++ server/db/migrations/000002_adding_new_feilds.up.sql
@@ -8,3 +8,9 @@ CREATE TABLE reviews_products (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(user_id, order_id)
 );
+
+
+-- ALTER TABLE reviews_products
+--     DROP CONSTRAINT reviews_user_order_unique,
+--     ADD CONSTRAINT reviews_user_order_product_unique
+--         UNIQUE (user_id, order_id, product_id);

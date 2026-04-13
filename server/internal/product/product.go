c22444d7d4af3218d53b5e245fccbee56549cd18
zuhaibdev <msuhaib637@gmail.com>
2026-04-13 23:19:41 +0300

    fixes

--- server/internal/product/product.go
+++ server/internal/product/product.go
@@ -19,42 +19,45 @@ type Product struct {
 }
 
 func (h *ProductsHandler) NewProducts(c fiber.Ctx) error {
-
 	var productData Product
-
 	if err := c.Bind().Body(&productData); err != nil {
-		c.Status(400).JSON(fiber.Map{
+		return c.Status(400).JSON(fiber.Map{
 			"message": "Invalid body request",
 		})
 	}
 
-	// validation inputs
 	if productData.Name == "" {
 		return c.Status(400).JSON(fiber.Map{
 			"message": "Name is required",
 		})
 	}
+
 	if productData.StockQuantity <= 0 {
 		return c.Status(400).JSON(fiber.Map{
-			"message": "Quantity must greater then 0",
+			"message": "Quantity must be greater than 0",
 		})
 	}
 
 	if !productData.Price.Valid {
-		return fmt.Errorf("price is required")
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Price is required",
+		})
 	}
 
 	val, err := productData.Price.Float64Value()
 	if err != nil {
-		return fmt.Errorf("invalid price format")
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid price format",
+		})
 	}
 
 	if val.Float64 <= 0 {
-		return fmt.Errorf("price must be greater than 0")
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Price must be greater than 0",
+		})
 	}
 
 	resid, err := GetUserResId(c, h)
-
 	if err != nil {
 		fmt.Println("DEBUG ERROR products: get user resId", err)
 		return c.Status(400).JSON(fiber.Map{
@@ -63,19 +66,21 @@ func (h *ProductsHandler) NewProducts(c fiber.Ctx) error {
 	}
 
 	params := db.NewProductParams{
-		RestaurantID: resid,
-		Name:         productData.Name,
-		Description:  utils.ToPgTex(productData.Description),
-		Price:        productData.Price,
-		Category:     utils.ToPgTex(productData.Category),
-		Images:       productData.Images,
-		StockQuantity: pgtype.Int4{
-			Int32: productData.StockQuantity,
-			Valid: true,
-		},
+		RestaurantID:  resid,
+		Name:          productData.Name,
+		Description:   utils.ToPgTex(productData.Description),
+		Price:         productData.Price,
+		Category:      utils.ToPgTex(productData.Category),
+		Images:        productData.Images,
+		StockQuantity: pgtype.Int4{Int32: productData.StockQuantity, Valid: true},
 	}
 
 	product, err := h.app.Query.NewProduct(c.Context(), params)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to create product",
+		})
+	}
 
 	return c.Status(201).JSON(fiber.Map{
 		"message": "Product successfully created",
